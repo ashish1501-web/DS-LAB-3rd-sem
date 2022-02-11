5ee5ea154df2791584e0ff05825669de105690df
ashish1501-web <84086198+ashish1501-web@users.noreply.github.com>
2022-02-11 11:49:22 +0530

    Update linked_list_lab5DS.c

--- linked_list_lab5DS.c
+++ linked_list_lab5DS.c
@@ -1,37 +1,47 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include<stdlib.h>
+
 struct node{
     int data;
     struct node* next;
-    
 };
 
 struct node* head=NULL;
 struct node* newNode;
 
-
-
-
-void insertAThead(int x)
-{   newNode=(struct node*) malloc(sizeof(struct node));
-    newNode->data=x;
-    newNode->next=head;
-    head=newNode;
+int count()
+{   int len=0;
+    struct node* temp=head;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        len++;
+    }
+    return len;
 }
 
-void insertATrear(int x)
-{   newNode=(struct node*) malloc(sizeof(struct node));
+void insert_beg(int x)
+{
     struct node* temp=head;
-    newNode->data=x;
+    newNode=(struct node*)malloc(sizeof(struct node*));
     newNode->next=NULL;
+    newNode->data=x;
+    if(head==NULL)
+    {
+        head=newNode;
+    }
+    else{
+        newNode->next=head;
+        head=newNode;
+    }
+}
+
+void insert_end(int x)
+{
+    struct node* temp=head;
+    newNode=(struct node*)malloc(sizeof(struct node*));
+    newNode->next=NULL;
+    newNode->data=x;
     if(head==NULL)
     {
         head=newNode;
@@ -42,90 +52,74 @@ void insertATrear(int x)
         temp=temp->next;
     }
     temp->next=newNode;
-    
-    
 }
 
-int size()
-{
+void insert_pos(int pos,int x)
+{   
     struct node* temp=head;
-    int len=0;
-    while(temp!=NULL)
-    {
-        len++;
-        temp=temp->next;
-    }
-    return len;
-}
-
-void insertATpos(int x,int pos)
-{   int len=size();
-    if(len==0)
-    {insertAThead(x);
-        return;
-        
-    }
-    if(pos>len+1)
-    {   printf("-----::position is out of range::----\n");
-        return;
-    }
-    if(pos==1)
-    {
-        insertAThead(x);
-        return;
-    }
-    if(pos==len+1)
-    {
-        insertATrear(x);
-        return;
-    }
-    newNode=(struct node*) malloc(sizeof(struct node));
+    newNode=(struct node*)malloc(sizeof(struct node*));
+   
     newNode->data=x;
-    struct node* temp=head;
-    for(int i=1;i<pos-1;i++)
-    {   
-        temp=temp->next;
+    if(pos==0)    
+    {
+        insert_beg(x);
     }
-    newNode->next=temp->next;
-    temp->next=newNode;
-    
+    else if(pos==count())
+    {
+        insert_end(x);
+    }
+    else{
+        for(int i=0;i<pos-1;i++)
+        {
+            temp=temp->next;
+        }
+        
+        newNode->next=temp->next;
+        temp->next=newNode;
+    }
 }
 
 void display()
 {
     struct node* temp=head;
+    
     while(temp!=NULL)
-    {
-        printf("%d\t",temp->data);
-        temp=temp->next;
+    {   printf("%d->",temp->data);
+         temp=temp->next;
     }
-    printf("\n");
-    
-    
 }
+
 int main()
 {
     int x,ch,pos;
-     printf("\nEnter::\n1.insertATpos\n2.deleteATpos\n3.display\n4.exit the program\n");
+     printf("\nEnter::\n1.insertATpos\n2.insert_beg\n3.insert_end\n4.display\n5.exit the program\n");
     while(1)
-    {   printf("Enter your choice:");
+    {   printf("\nEnter your choice:");
         scanf("%d",&ch);
         switch(ch)
         {
             case 1:
                 printf("Enter element and position to insert in linked list:");
                 scanf("%d %d",&x,&pos);
-                insertATpos(x,pos);
+                insert_pos(pos,x);
+                break;
+            case 2:
+                 printf("Enter element insert in linked list:");
+                scanf("%d",&x);
+                insert_beg(x);
                 break;
             case 3:
-                display();
+                 printf("Enter element insert in linked list:");
+                scanf("%d",&x);
+                insert_end(x);
                 break;
             case 4:
+                display();
+                break;
+            case 5:
                 exit(0);
                 break;
         }
     }
     return 0;
 }
-
-
