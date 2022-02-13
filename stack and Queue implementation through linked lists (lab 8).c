f3a465f765ed49260ae63d6692a92c76ca34d839
ashish1501-web <84086198+ashish1501-web@users.noreply.github.com>
2022-02-13 11:05:01 +0530

    Create stack and Queue implementation through linked lists (lab 8).c

---
+++ stack and Queue implementation through linked lists (lab 8).c
@@ -0,0 +1,116 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+    
+};
+
+struct node* head=NULL;
+
+
+struct node* newNode;
+
+void insertAThead(int x)
+{   newNode=(struct node*) malloc(sizeof(struct node));
+    newNode->data=x;
+    newNode->next=head;
+    head=newNode;
+}
+
+
+void deleteATend()
+{
+    struct node* temp=head,*prev=head;
+    if(head==NULL)
+    {
+        printf("No element is present to delete");
+        return;
+    }
+    else if(head->next==NULL)
+    {   head=NULL;
+        free(temp);
+    }
+    while(temp->next!=NULL)
+    {
+        prev=temp;
+        temp=temp->next;
+    }
+    free(temp);
+    prev->next=NULL;
+
+}
+
+void deleteAThead()
+{   struct node* temp=head;
+    if(head==NULL)
+    {
+        printf("No element is present to delete");
+        return;
+    }
+    head=head->next;
+    free(temp);
+}
+void display()
+{
+    struct node* temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d\t",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+    
+    
+}
+
+int main()
+{   int val,ch;
+    
+    while(1)
+    {  printf("Enter\n 1.push into stack\n 2.pop from stack\n 3.display\n 4.enqueue\n 5.dequeue\n 6.display queue elements \n 7.exit");
+        printf("\nEnter choice:");
+        scanf("%d",&ch);
+        switch(ch)
+    {   case 1:
+         {   printf("enter the value:");
+            scanf("%d",&val);
+            insertAThead(val);
+            break;}
+          case 2:
+            
+            
+           deleteAThead(val);
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+         {   printf("enter the value:");
+            scanf("%d",&val);
+            insertAThead(val);
+            break;}
+          case 5:
+            
+            
+           deleteATend(val);
+            break;
+        case 6:
+            display();
+            break;
+        case 7:
+           exit(0);
+        
+    }
+    }
+    
+}
+
